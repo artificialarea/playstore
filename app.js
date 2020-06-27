0d263204a922849947a532c93f8afe1d30947f3a
Alen Diaz <noricual@gmail.com>
2020-06-27 14:22:40 -0500

    endpoint and basic query implemented

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
+const playstore = require("./playstore");
+const { query } = require("express");
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/', (req, res) => {
+app.get("/apps", (req, res) => {
+  const { sort, genres } = req.query;
+  let results = playstore
+  if (sort) {
+    if (!["rating", "app"].includes(sort)) {
+      return res.status(400).send("Sort must be by rating or app");
+    }
+  }
 
-  res.send('Yo MTV Raps!')
+  if (sort) {
+    results.sort((a, b) => {
+      return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+    });
+  }
 
-
-
-})
+  res.json(results);
+});
 
 app.listen(8080, () => {
-  console.log('Express server is listening on 8080...')
-})
+  console.log("Express server is listening on 8080...");
+});
