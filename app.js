f5eda374a4f62a8f8993086c30823081dbbafd61
Sacha Sedriks <sachased@gmail.com>
2020-06-27 13:56:47 -0700

    modified sort + genre criteria so it works within certain spelling (lowercase for genres and captialized for sort). May refactor later so works in any permutation of uppercase/lowercase.

--- app.js
+++ app.js
@@ -11,21 +11,26 @@ app.get("/apps", (req, res) => {
   const { sort, genres } = req.query;
   let results = playstore;
 
+  // make all keys in playstore/results lowercase
+  Object.keys(results).forEach(function(key) {
+    console.log(key);
+  });
+
   if (sort) {
-    if (!["rating", "app"].includes(sort)) {
-      return res.status(400).send("Sort must be by rating or app");
+    if (!["Rating", "App"].includes(sort)) {
+      return res.status(400).send("Sort must be by Rating or App");
     }
   }
 
   if (genres) {
-    if (!["action", "puzzle", "strategy", "sasual", "arcade", "card"].includes(genres)) {
+    if (!["action", "puzzle", "strategy", "casual", "arcade", "card"].includes(genres)) {
       return res.status(400).send("Genre must be either Action, Puzzle, Strategy, Casual, Arcade, or Card.");
     }
   }
 
   if (genres) {
-    console.log('genres invoked')
-    console.log(genres)
+    // console.log('genres invoked')
+    // console.log(genres)
     results = results.filter(game =>
       game
         .Genres
@@ -37,7 +42,7 @@ app.get("/apps", (req, res) => {
 
   if (sort) {
     console.log(sort);
-    sort = sort.toLowerCase
+    // sort = sort.toLowerCase
     results = results.sort((a, b) => {
       return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
     });
