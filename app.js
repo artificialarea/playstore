b820dfbc48cfad506d3e6e725f62e756e16d8a6f
Sacha Sedriks <sachased@gmail.com>
2020-06-27 13:25:40 -0700

    integrated sort and genre query handlers, but non-functional.

--- app.js
+++ app.js
@@ -9,20 +9,45 @@ app.use(morgan("dev"));
 
 app.get("/apps", (req, res) => {
   const { sort, genres } = req.query;
-  let results = playstore
+  let results = playstore;
+
   if (sort) {
     if (!["rating", "app"].includes(sort)) {
       return res.status(400).send("Sort must be by rating or app");
     }
   }
 
+  if (genres) {
+    if (!["action", "puzzle", "strategy", "sasual", "arcade", "card"].includes(genres)) {
+      return res.status(400).send("Genre must be either Action, Puzzle, Strategy, Casual, Arcade, or Card.");
+    }
+  }
+
+  if (genres) {
+    console.log('genres invoked')
+    console.log(genres)
+    results = results.filter(game =>
+      game
+        .Genres
+        .toLowerCase()
+        .includes(genres.toLowerCase())
+    );
+  }
+  
+
   if (sort) {
-    results.sort((a, b) => {
+    console.log(sort);
+    sort = sort.toLowerCase
+    results = results.sort((a, b) => {
       return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
     });
   }
 
+
   res.json(results);
+
+
+
 });
 
 app.listen(8080, () => {
