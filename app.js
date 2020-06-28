508b187978d6c4de7d567d6096b795cd7237fe5b
Sacha Sedriks <sachased@gmail.com>
2020-06-27 22:19:39 -0700

    version with database keys converted to all lowercase.

--- app.js
+++ app.js
@@ -9,6 +9,36 @@ app.use(morgan("dev"));
 app.get("/apps", (req, res) => {
   const { sort, genres, search = '' } = req.query;
 
+  // probably an overkill
+  // but converting keys of the objects in the playstore dataset array to *lowercase*
+  // so the values of the sort query (ratings and app) can be lowercase (or even a mixthereof)
+
+  // v2 keys to lowercase (more elegant solution, but a blackbox to me)
+  const playstoreLowerCase = playstore.map(obj => {
+    return Object.keys(obj).reduce(function(accum, key) {
+      accum[key.toLowerCase()] = obj[key];
+      return accum;
+    }, {});
+  })
+
+  // v1 keys to lowercase
+  // unfortunately my solution didn't consistently work. troubleshooting exceed timebox.
+  // function keysToLowerCase (obj) {
+  //   let keys = Object.keys(obj);
+  //   for (let i = 0; i < keys.length; i++) {
+  //     let key = keys[i];
+  //     obj[key.toLowerCase()] = obj[key]
+  //     delete obj[key]
+  //   }
+  //   return (obj);
+  // }
+  // const playstoreLowerCase = playstore.map(obj => {
+  //   return keysToLowerCase(obj)
+  // })
+
+  console.log(playstore)
+  console.log(playstoreLowerCase)
+
   // VALIDATION
   if (sort) {
     if (!["rating", "app"].includes(sort.toLowerCase())) {
@@ -23,26 +53,38 @@ app.get("/apps", (req, res) => {
   }
 
   // if search
-  let results = playstore
+  let results = playstoreLowerCase
     .filter(game => 
       game
-        .App
+        .app
         .toLowerCase()
         .includes(search.toLowerCase()));
 
   if (genres) {
     results = results.filter(game =>
       game
-        .Genres
+        .genres
         .toLowerCase()
         .includes(genres.toLowerCase())
     );
   }
   
   if (sort) {
+    
+    const sortLowerCase = sort.toLowerCase();
+    console.log(sortLowerCase)
     results = results.sort((a, b) => {
-      return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+        // console.log(a[sortLowerCase], b[sortLowerCase])
+        // return a[sortLowerCase] > b[sortLowerCase] ? 1 : a[sortLowerCase] < b[sortLowerCase] ? -1 : 0;
+        return ( 
+          a[sortLowerCase] > b[sortLowerCase] ? 
+            (sortLowerCase === 'app' ? 1 : -1)     // if app: a to z , else ratings: hi to lo
+          : a[sortLowerCase] < b[sortLowerCase] ? 
+            (sortLowerCase === 'app' ? -1 : 1) 
+          : 0
+        )
     });
+
   }
 
   res.json(results);
