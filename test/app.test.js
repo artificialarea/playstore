16df14a6114f17dec85fce73e2abfcf39641dac7
Sacha Sedriks <sachased@gmail.com>
2020-06-30 14:27:40 -0700

    added response 200 supertests for sort (by App & Rating) and filter (by Genre).

--- test/app.test.js
+++ test/app.test.js
@@ -4,25 +4,26 @@ const supertest = require('supertest');   // tool for testing HTTP calls
 
 const app = require('../app');
 const playstore = require('../playstore');
+const { get } = require('../app');
 
 // POSSIBLE TESTS for /books endpoint...
-// response 200 array exists
-// response 400 if sort query values incorrect
-// response 400 if genre query values incorrect
-// response 200 if sort query App sorts array by title alphabetically from A-Z
-// response 200 if sort query Ratings sorts array from highest to lowest ratings
-// response 200 if genre query filters array appropriately
-// response 200 if search query returns appropriate matches
-// response ___ if search query returns no matches (not 4xx... 2xx or 1xx?)
+// [x] response 200 array exists
+// [x] response 400 if sort query values incorrect
+// [x] response 400 if genre query values incorrect
+// [x] response 200 if sort query App sorts array by title alphabetically from A-Z
+// [x] response 200 if sort query Ratings sorts array from highest to lowest ratings
+// [x] response 200 if genre query filters array appropriately
+// [ ] response 200 if search query returns appropriate matches
+// [ ] response ___ if search query returns no matches (not 4xx... 2xx or 1xx?)
 // 
 
 describe('GET /apps endpoint', () => {
 
-  it('returns the full playstore array with no query', () => {
+  it('should return full playstore array with no query', () => {
     return supertest(app)
       .get('/apps')
-      .expect('Content-Type', /json/)
       .expect(200)
+      .expect('Content-Type', /json/)
       .then(res => {
         // console.log(res.body);
         // make sure you get an array
@@ -34,6 +35,87 @@ describe('GET /apps endpoint', () => {
       });
   });
 
+  it('should 400 if sort parameters incorrect', () => {
+    return supertest(app)
+      .get('/apps')
+      // .query({ sort: !['Rating', 'App'] }) // works, too
+      .query({ sort: 'MISTAKE' })
+      .expect(400, `Sort must be by 'Rating' or 'App'`)
+  });
+
+  it('should 400 if genre parameters incorrect', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ genres: 'MISTAKE'})
+      .expect(400, "Genre must be either Action, Puzzle, Strategy, Casual, Arcade, or Card.")
+  });
+
+  it('should sort alphabetically by App title from A to Z', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ sort: 'App'})
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('array')
+        let sorted = true;
+        let i = 0;
+        while (i < res.body.length - 1){
+          const appsAtI = res.body[i];
+          const appsAtIPlus = res.body[i + 1];
+          if(appsAtIPlus.Apps < appsAtI.Apps) {
+            sorted = false;
+            break;
+          }
+          i++;
+        }
+        expect(sorted).to.be.true;
+      })
+  });
+  
+  it('should sort by Rating from highest to lowest', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ sort: 'Rating'})
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('array')
+        let sorted = true;
+        let i = 0;
+        while( i < res.body.length - 1){
+          const appsAtI = res.body[i];
+          const appsAtIPlus = res.body[i + 1];
+          if(appsAtIPlus.Rating > appsAtI.Rating) {
+            sorted = false;
+            break;
+          }
+          i++;
+        }
+        expect(sorted).to.be.true;
+      })
+  });
+
+  it('should filter by genre', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ genres: 'Casual' })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        let filtered = true;
+        let i = 0;
+        while (i < res.body.length) {
+          // console.log(res.body[i].Genres);
+          if(!(res.body[i].Genres).includes('Casual')) {
+            filtered = false;
+            break;
+          }
+          i++;
+        }
+        expect(filtered).to.be.true;
+      })
+  });
 });
 
 
